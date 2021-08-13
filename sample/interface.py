2640b4c391f586e7f073b666c16f901323aad59a
Chau Laurent <byTrickee@gmail.com>
2021-08-13 10:30:54 +0200

    update interface

--- sample/interface.py
+++ sample/interface.py
@@ -5,6 +5,7 @@ Interface utilisateur
 Fonctions:
 
 """
+import random
 from .context import definitions, transformations, lectures, ecritures
 
 
@@ -20,6 +21,7 @@ def interface() -> None:
     print("\t3. Transformation d'une partition")
     print("\t4. Composition d'une nouvelle partition")
     print("\t5. Enrichir la base de donnee avec de nouvelles partitions")
+    print("\t6. Quitter")
     choix = int(input("Choix : "))
 
     if choix == 1:
@@ -32,31 +34,35 @@ def interface() -> None:
         print("\t2. Inversion")
         choix_transfo = int(input("Choix : "))
 
-        print("Choisissez parmi :")
-        with open("docs/partitions/partitions.txt") as f:
-            for ligne in f.readlines()[::2]:
-                print("\t" + ligne.strip()[1::])
-        choix_partition = int(input("Choix : "))
+        if choix_transfo == 1 or choix_transfo == 2:
+            print("Choisissez parmi :")
+            with open("docs/partitions/partitions.txt") as f:
+                for ligne in f.readlines()[::2]:
+                    print("\t" + ligne.strip()[1::])
+            choix_partition = int(input("Choix : "))
 
-        ligne = definitions.read_line_file(
-            "docs/partitions/partitions.txt", choix_partition * 2
-        )
-        seq_freq, seq_duree = definitions.read_sheet(ligne)
+            ligne = definitions.read_line_file(
+                "docs/partitions/partitions.txt", choix_partition * 2
+            )
+            seq_freq, seq_duree = definitions.read_sheet(ligne)
+        else:
+            print("Choix incorrect !\n")
 
         if choix_transfo == 1:
             k = int(input("De combien voulez-vous transposer ? "))
             transfo_freq = transformations.transposition(seq_freq, k)
             print("Partition transposee : ")
             lectures.lecture_transfo(transfo_freq, seq_duree)
-        else:
+        elif choix_transfo == 2:
             transfo_freq = transformations.inversion(seq_freq)
             print("Partition inversee : ")
             lectures.lecture_transfo(transfo_freq, seq_duree)
 
-        lecture = int(input("Voulez-vous la lire ? (1/0) "))
+        if choix_transfo == 1 or choix_transfo == 2:
+            lecture = int(input("Voulez-vous la lire ? (1/0) "))
 
-        if lecture:
-            definitions.play_sheet(seq_duree, transfo_freq)
+            if lecture:
+                definitions.play_sheet(seq_duree, transfo_freq)
     elif choix == 4:
         print("Choisissez parmi :")
         print("\t1. Application des chaines de Markov sur une partition")
@@ -65,7 +71,12 @@ def interface() -> None:
         )
         choix_markov = int(input("Choix : "))
 
-        len_partition = int(input("Quelle est la taille de la nouvelle partition ? "))
+        if choix_markov == 1 or choix_markov == 2:
+            len_partition = int(
+                input("Quelle est la taille de la nouvelle partition ? ")
+            )
+        else:
+            print("Choix incorrect !\n")
 
         if choix_markov == 1:
             print("Choisissez parmi :")
@@ -82,15 +93,26 @@ def interface() -> None:
             transfo_freq = ecritures.markov_1(seq_freq, len_partition)
             print("Nouvelle partition : ")
             lectures.lecture_transfo(transfo_freq, seq_duree)
-        else:
+        elif choix_markov == 2:
             transfo_freq = ecritures.markov_2(len_partition)
+            seq_duree = [
+                random.choice(
+                    list(
+                        definitions.calc_duration(
+                            definitions.figures, definitions.d0
+                        ).values()
+                    )
+                )
+                for _ in transfo_freq
+            ]
             print("Nouvelle partition : ")
             lectures.lecture_transfo(transfo_freq, seq_duree)
 
-        lecture = int(input("Voulez-vous la lire ? (1/0) "))
+        if choix_markov == 1 or choix_markov == 2:
+            lecture = int(input("Voulez-vous la lire ? (1/0) "))
 
-        if lecture:
-            definitions.play_sheet(seq_duree, transfo_freq)
+            if lecture:
+                definitions.play_sheet(seq_duree, transfo_freq)
     elif choix == 5:
         print(
             "Mettez votre fichier partition dans docs/partition et verifier que c'est un fichier texte (.txt)"
@@ -102,6 +124,9 @@ def interface() -> None:
             print("Le fichier n'existe pas")
         else:
             ecritures.ajout_partition(fichier_name)
+    elif choix == 6:
+        print("Au revoir !\n")
+        return None
     else:
         print("Choix incorrect !\n")
 
